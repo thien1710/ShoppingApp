0d7f12f57ecc9c82fd8f6ad7e7cd07e06b6911e7
thien <nguyentrannhatthien1710@gmail.com>
2020-03-24 22:51:34 +0700

    header style

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput, Dimensions } from 'react-native';
 
 import Authentication from '../Authentication/Authentication';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
+const { height } = Dimensions.get('window');
 export default class Main extends Component {
     render() {
         return (
-            <View style={styles.container1}>
+            <View style={styles.container}>
                 <View style={styles.header}>
-                    <Text>flex1</Text>
-                    <Button title="Toggle drawer" onPress={() => this.props.navigation.toggleDrawer()} />
+                    <TouchableOpacity onPress={() => this.props.navigation.toggleDrawer()}>
+                        <Image source={require('../../media/appIcon/ic_menu.png')}
+                            style={styles.iconStyle} />
+                    </TouchableOpacity>
+                    <Text style={styles.titleStyle}>Wearing a Dress</Text>
+                    <Image source={require('../../media/appIcon/ic_logo.png')}
+                        style={styles.iconStyle} />
                 </View>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="What do you want to buy?"
+                />
+
                 <View style={styles.main}>
                     <Text>This is Main</Text>
                     <Button
@@ -25,58 +37,33 @@ export default class Main extends Component {
 }
 
 const styles = StyleSheet.create({
-    container1: {
+    container: {
         flex: 1,
+        height: height / 8,
         backgroundColor: '#ffff22',
+        justifyContent: 'space-around',
+        padding: 10,
     },
     header: {
-        flex: 1,
-        backgroundColor: 'red',
-        alignItems: 'center',
-        justifyContent: 'center'
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     main: {
         backgroundColor: 'blue',
         flex: 4
     },
-    buttons: {
-        flex: 7,
-        flexDirection: 'row',
+    textInput: {
+        backgroundColor: '#FFF',
+        paddingLeft: 10,
+        height: height / 23,
     },
-    numbers: {
-        flex: 3,
-        backgroundColor: '#f3f3f3',
+    titleStyle: {
+        color: '#FFF',
+
+        fontSize: 20,
     },
-    operations: {
-        flex: 1,
-        backgroundColor: '#edeaea',
-        justifyContent: 'space-around',
-        alignItems: 'stretch'
-    },
-    row: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center'
-    },
-    resultText: {
-        fontSize: 30,
-        color: 'black',
-    },
-    calculationText: {
-        fontSize: 24,
-        color: 'black',
-    },
-    btn: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignSelf: 'stretch',
-    },
-    btnText: {
-        fontSize: 30,
-    },
-    btnWhite: {
-        color: 'black',
+    iconStyle: {
+        width: 25,
+        height: 25,
     },
 });
