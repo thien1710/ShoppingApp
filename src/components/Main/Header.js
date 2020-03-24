061753d8ac9f4fd7482f0461808dfb7f853d661f
thien <nguyentrannhatthien1710@gmail.com>
2020-03-24 12:40:09 +0700

    complete header and main in Main.js

--- src/components/Main/Header.js
+++ src/components/Main/Header.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default class Header extends Component {
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Authentication from '../Authentication/Authentication'
+
+export default class Header extends Component  {
     render() {
         return (
-                <View style={{ flex: 1, backgroundColor: 'red', width: 100 }}>
-                    <Text>Text 2here</Text>
-                </View>
+            <View style={{ flex: 1, backgroundColor: 'red', width: 100 }}>
+                <Button title="Toggle drawer" onPress={() => this.props.navigation.toggleDrawer()} />
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-  });
-  
+});
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,22 +2,81 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 import Authentication from '../Authentication/Authentication';
-import Header from './Header'
 
 export default class Main extends Component {
     render() {
         return (
-            <View style={{flexDirection:'column', flex:1}}>
-                <Header/>
-                <View style={{backgroundColor:'blue', flex:4, width: 100}}>
+            <View style={styles.container1}>
+                <View style={styles.header}>
+                    <Text>flex1</Text>
+                    <Button title="Toggle drawer" onPress={() => this.props.navigation.toggleDrawer()} />
+                </View>
+                <View style={styles.main}>
                     <Text>This is Main</Text>
                     <Button
                         title="Go to Authentication"
                         onPress={() => { this.props.navigation.navigate('Authentication') }}
                     />
-                {/* <Button title="Toggle drawer" onPress={() => this.props.navigation.toggleDrawer()} /> */}
+                    <Text>flex4</Text>
                 </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container1: {
+        flex: 1,
+        backgroundColor: '#ffff22',
+    },
+    header: {
+        flex: 1,
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    main: {
+        backgroundColor: 'blue',
+        flex: 4
+    },
+    buttons: {
+        flex: 7,
+        flexDirection: 'row',
+    },
+    numbers: {
+        flex: 3,
+        backgroundColor: '#f3f3f3',
+    },
+    operations: {
+        flex: 1,
+        backgroundColor: '#edeaea',
+        justifyContent: 'space-around',
+        alignItems: 'stretch'
+    },
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+    resultText: {
+        fontSize: 30,
+        color: 'black',
+    },
+    calculationText: {
+        fontSize: 24,
+        color: 'black',
+    },
+    btn: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+    },
+    btnText: {
+        fontSize: 30,
+    },
+    btnWhite: {
+        color: 'black',
+    },
+});
